ba8317ed0371e905a5ed001d95bf62bf9fb955ac
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-01-19 11:55:49 +0530

    Add: Store the Day and the Daily Wage along with the Total Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -9,6 +9,7 @@ empRatePerHr=20;
 numWorkingDays=20;
 day=1;
 
+declare -A day
 function calculateWorkingHour() {
 	case $randomCheck in 
                      $isFullTime)
@@ -37,5 +38,6 @@ randomCheck=$((RANDOM%3));
 	((day++));
 echo "Daily Wage: " ${salary[*]}
 echo "Total Wage: " ${totalSalary[*]}
+echo "Day: " ${day[*]}
 done
 echo "Employee has earned $totalSalary $ in a month (Total working Hour : $totalWorkingHour)";
